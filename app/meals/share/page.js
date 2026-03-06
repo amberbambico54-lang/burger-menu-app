52c36c046e0ce2f3034ee89dfd0e9b2855755f85
graziella01 <amberbambico.54@gmail.com>
2026-03-06 14:52:44 +0800

    Refactor code structure for improved readability and maintainability

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,9 +1,14 @@
+"use client";
+
 import { shareMeal } from "@/lib/action";
 import styles from "./page.module.css";
 import ImagePicker from "@/components/meals/ImagePicker";
 import MealsFormSubmit from "@/components/meals/meals-form-submit";
+import { useFormState } from "react-dom";
 
 export default function ShareMealPage() {
+  const [state, formAction] = useFormState(shareMeal, { message: null });
+
   return (
     <>
       <header className={styles.header}>
@@ -13,7 +18,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={styles.main}>
-        <form className={styles.form} action={shareMeal}>
+        <form className={styles.form} action={formAction}>
           <div className={styles.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -43,6 +48,7 @@ export default function ShareMealPage() {
           </p>
           {/* IMAGE PICKER */}
           <ImagePicker label="Your image" name="image" />
+          {state.message && <p>{state.message}</p>}
           <p className={styles.actions}>
             <MealsFormSubmit />
           </p>
